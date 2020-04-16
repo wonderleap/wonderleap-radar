943d3d1c0f38888a5c73e96bd6ed3e4456b71544
luis_freitas <luis.freitas344@gmail.com>
2020-04-16 21:09:11 +0100

    Added chart

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts'
 
 import request from '../utils/graphql'
 
@@ -10,6 +11,11 @@ const getAppInfo = `query GetData($id: uuid) {
     sidequest_apps (where: {id: {_eq: $id}}) {
         name
         date_added
+        sq_id
+        records {
+          created_at
+          downloads
+        }
     }
 }`
 
@@ -40,18 +46,45 @@ class App extends React.Component {
   render() {
     const { app } = this.state
 
-    console.log(app)
-
     return (
       <Layout>
-        {!app ? (
-          <p>App does not exist</p>
-        ) : (
-          <div>
-            <h1> {app.name} </h1>
-            <p> {app.date_added} </p>
-          </div>
-        )}
+        <div>
+          <section className="text-center">
+            <div className="container">
+              {!app ? (
+                <p>App does not exist</p>
+              ) : (
+                <div>
+                  <a href={`https://sdq.st/a-${app.sq_id}`}>
+                    <h1> {app.name} </h1>
+                  </a>
+                  <p> First released on {app.date_added} </p>
+                </div>
+              )}
+
+              <div className="col-xs-1" align="center">
+                <LineChart
+                  width={400}
+                  height={400}
+                  data={app.records}
+                  margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+                >
+                  <XAxis dataKey="created_at" />
+                  <YAxis dataKey="downloads" />
+                  <Tooltip />
+                  <CartesianGrid stroke="#f5f5f5" />
+                  <Line
+                    type="monotone"
+                    dataKey="downloads"
+                    stroke="#ff7300"
+                    yAxisId={0}
+                  />
+                  {/*<Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />*/}
+                </LineChart>
+              </div>
+            </div>
+          </section>
+        </div>
       </Layout>
     )
   }
