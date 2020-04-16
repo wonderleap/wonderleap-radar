e2ff00468cbe495b8cfea9033db673b01ce4f93e
luis_freitas <luis.freitas344@gmail.com>
2020-04-16 19:44:39 +0100

    Move to axios insted of fetch

--- src/utils/graphql.js
+++ src/utils/graphql.js
@@ -1,19 +1,20 @@
+const axios = require('axios')
+
 const request = ({ headers, query, variables }) => {
-  return fetch('https://annie-vr.herokuapp.com/v1/graphql', {
+  return axios({
     method: 'POST',
+    url: 'https://annie-vr.herokuapp.com/v1/graphql',
     headers: {
       ...headers,
       'Content-Type': 'application/json',
       Accept: 'application/json',
       'x-hasura-admin-secret': 'sidequest_snapshots',
     },
-    body: JSON.stringify({
+    data: {
       query,
       variables,
-    }),
-  })
-    .then(r => r.json())
-    .then(_data => _data)
+    },
+  }).then(_data => _data.data)
 }
 
 export default request
